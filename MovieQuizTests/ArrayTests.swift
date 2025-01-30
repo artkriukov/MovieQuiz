4043c503ef693075935bc584a41512ea75204880
Artem Kriukov <art.kriukov@gmail.com>
2025-01-30 18:28:29 +0300

    add ArrayTest and MoviesLoaderTests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -5,4 +5,30 @@
 //  Created by Artem Kriukov on 30.01.2025.
 //
 
-import Foundation
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws { // тест на успешное взятие элемента по индексу
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 2]
+        
+        // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
+    } 
+}
