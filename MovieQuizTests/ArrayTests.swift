effe720fb80c88e9f2c95ed8f09203bfb245754f
Artem Kriukov <art.kriukov@gmail.com>
2025-01-30 18:28:13 +0300

    add ArrayTest and MoviesLoaderTests

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Artem Kriukov on 30.01.2025.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Artem Kriukov on 30.01.2025.
+//
+
+import Foundation
